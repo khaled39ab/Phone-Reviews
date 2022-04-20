8f65b185b9f43da7317bc6f9f090b844b497b557
khaled39ab <khaled39ab@gmail.com>
2022-04-20 19:21:00 +0600

    Bonus part done

--- JS/app.js
+++ JS/app.js
@@ -62,23 +62,38 @@ const loadDetail = (phoneId) => {
 
 /* =========================     display details      ========================= */
 const displayDetail = (detail) => {
+    console.log(detail);
+    detailSection.textContent = ""
     const div = document.createElement('div');
     div.classList.add('row');
     div.innerHTML = `
-    <div class="card mb-3" style="max-width: 540px;">
-        <div class="row g-0">
-            <div class="col-md-4">
-                <img src="${detail.image}" class="img-fluid rounded-start" alt="...">
+        <div class="row">
+            <div class="col">
+                <div class="card mb-3" style="max-width: 540px; height:220px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="${detail.image}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title" style="color:#ff4000">${detail.name}</h5>
+                                <h6 style="color:steelBlue">${detail.releaseDate ? detail.releaseDate : 'No result'}</h6>
+                                <p class="card-text"><span style="color:red;font-weight:bold">Feature:</span> ${detail.mainFeatures.storage} . ${detail.mainFeatures.chipSet} . ${detail.mainFeatures.displaySize} . ${detail.mainFeatures.memory}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="col">
+                <div class="card" style="width: 540px;height:220px;">
+                    <div class="card-body">
+                        <h5 class="card-title">Sensors & More</h5>
+                        <p class="card-text"><span style="color:red;font-weight:bold">Sensors: </span>${detail.mainFeatures.sensors}</p>
+                        <p class="card-text"><span style="color:red;font-weight:bold">Others: </span>Bluetooth: ${detail.others.Bluetooth ? detail.others.Bluetooth : 'No result found'} #GPS: ${detail.others.GPS ? detail.others.GPS :'No result found'} #NFC: ${detail.others.NFC ? detail.others.NFC :'No result found'} #WLAN: ${detail.others.WLAN ? detail.others.WLAN :'No result found'} #Radio: ${detail.others.Radio} #USB: ${detail.others.USB ? detail.others.USB :'No result found'}</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     `;
     detailSection.appendChild(div)
 }
